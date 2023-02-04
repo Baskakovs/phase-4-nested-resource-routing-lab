ea12fed63cdb6a34a9d96f6bc85851b3f3c38508
Baskakovs <baskakovs@me.com>
2023-02-04 16:39:59 +0000

    commit tree 7b41f68de2f0616bc53e71af8ee6083f39710a5f

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,28 @@
 class ItemsController < ApplicationController
-
+rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
   def index
-    items = Item.all
-    render json: items, include: :user
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      items = user.items
+    else
+      items = Item.all
+   end
+   render json: items, include: :user
   end
 
+  def show 
+    item = Item.find(params[:id])
+    render json: item
+  end
+
+  def create
+    item = Item.create(name: params[:name], description: params[:description], price: params[:price], user_id: params[:user_id])
+    render json: item, status: :created
+  end
+
+  private 
+
+  def record_not_found
+    render plain: "404 Not Found", status: 404
+  end
 end
